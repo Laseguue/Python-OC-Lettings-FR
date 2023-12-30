15bca0c60107d876b82423cc7770d2979605497b
GhostDog <ghostdog@macbook.home>
2023-12-30 18:40:40 +0100

    test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,6 +17,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
+
 ALLOWED_HOSTS = ['*']
 
 # Application definition
